0e613f30a14ca1d608d270cad03b27b2b1cc1624
Jadi <jadijadi@gmail.com>
2018-03-31 14:11:25 +0430

    header and adding search too google url

--- l_files/bundle.js
+++ l_files/bundle.js
@@ -447,7 +447,7 @@ $(function(){
     function redirect(){
       if ($.getQueryString({ id: "debug" })) return;
 
-      var google = "http://google.com/?q=";
+      var google = "http://google.com/search?q=";
       if (button.attr("id") == $("#lucky").attr("id")) {
         google = "http://www.google.com/search?hl=en&btnI=I%27m+Feeling+Lucky&pws=0&q=";
       }

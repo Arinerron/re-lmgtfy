534c5625dbc779b0a221cd26259e00e8d6342ffe
Ashkan Rahmani <ashkan.rmk@hotmail.com>
2018-04-02 15:41:46 +0430

    Add go to link button

--- js/clipboard.js
+++ js/clipboard.js
@@ -11,3 +11,9 @@ function outFunc() {
     var tooltip = document.getElementById("myTooltip");
     tooltip.innerHTML = "ذخیره در کلیپبورد";
 }
+
+function openInNewTab() {
+    var url = document.getElementById("myInput").value;
+    var win = window.open(url, '_blank');
+    win.focus();
+}
